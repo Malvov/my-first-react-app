66759b29cf5a17cda75bde13fac11a5c82b9f99e
Manuel Olivares <manuel.olivares@envato.com>
2023-10-15 18:53:47 -0600

    more

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import ExpenseItem from './components/ExpenseItem';
+import Expense from './components/Expense';
 
 function App() {
   const expenses = [
@@ -27,10 +27,7 @@ function App() {
 
   return (
     <div>
-      <h1>My React app</h1>
-      <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-      <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-      <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
+      <Expense expenses={expenses} />
     </div>
   );
 }
